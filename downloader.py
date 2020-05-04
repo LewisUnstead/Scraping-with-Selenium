1636cf5604e572a6850e6d69658bc69bbbc3f430
Nish Shyam <nshyam668@gmail.com>
2020-05-04 21:33:38 +0100

    Added github code to update file

--- downloader.py
+++ downloader.py
@@ -1,8 +1,9 @@
 import requests
 import os
 from selenium import webdriver
-
-wd = webdriver.Chrome()
+from github import Github
+import secrets
+import pandas
 
 URL = 'https://www.apple.com/covid19/mobility'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) '
@@ -14,15 +15,29 @@ def download_csv(download_link):
     with open(os.path.join("/Users/nishanthshyam/Documents/Apple-Mobility-Downloader", "applemobility.csv"),
               'wb') as f:
         f.write(test.content)
+    return test.content
 
 
-wd.get(URL)
-html = wd.page_source
-links = wd.find_elements_by_tag_name('a')
+def scrape_website():
+    wd = webdriver.Chrome()
+    wd.get(URL)
+    links = wd.find_elements_by_tag_name('a')
 
-for link in links:
-    csv_data = link.get_attribute('href')
-    if 'mobility' in csv_data:
-        download_csv(csv_data)
+    for link in links:
+        csv_data = link.get_attribute('href')
 
-wd.quit()
+        if 'mobility' in csv_data:
+            content = download_csv(csv_data)
+            send_to_github(content)
+
+    wd.quit()
+
+
+def send_to_github(file):
+    g = Github(secrets.token)
+    repo = g.get_repo('nshyam97/Apple-Mobility-Trends-Data')
+    contents = repo.get_contents('applemobility.csv')
+    repo.update_file(contents.path, 'file update', file, contents.sha, branch='master')
+
+
+scrape_website()
